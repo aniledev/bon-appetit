2372ef9f9e1499e2e7ac65f55c3cacfcfad3108e
aniledev <elinamcgill@gmail.com>
2021-04-10 14:40:02 -0400

    FEAT: write up context to use Context.Provider with context values

---
+++ src/context.js
@@ -0,0 +1,20 @@
+import React, { useState, createContext } from "react";
+
+export const Context = createContext();
+export const ContextProvider = (props) => {
+  const { restaurants, setRestaurants } = useState([]);
+  return (
+    // the context provider needs to hold the value of restaurants from the api
+    // setRestaurants is a function that updates the state with the restaurant response data
+    <Context.Provider value={{ restaurants, setRestaurants }}>
+      {props.children}
+    </Context.Provider>
+  );
+};
+// don't forget to wrap the App component in the context so that it can consume the context values
+
+// // use createContext to initialize context for use in application
+
+// export default React.createContext({
+//   // empty update functions
+// });
