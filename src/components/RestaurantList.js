896c324efdcc71702227b19588689a9b23bc2036
aniledev <elinamcgill@gmail.com>
2021-04-10 13:36:18 -0400

    FEAT: create children componenents for Home Route

---
+++ src/components/RestaurantList.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class RestaurantList extends Component {
+  render() {
+    return <div>Restaurant List for Home Route</div>;
+  }
+}
--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import Header from "../components/Header";
+import AddRestaurantForm from "../components/AddRestaurantForm";
+import RestaurantList from "../components/RestaurantList";
 
 export default class HomeRoute extends Component {
   render() {
-    return <div>Home Route</div>;
+    return (
+      <div>
+        <Header />
+        <AddRestaurantForm />
+        <RestaurantList />
+      </div>
+    );
   }
 }
