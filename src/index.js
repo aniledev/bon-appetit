a58fd76a9d0977b1b26fabc32e2436cfabc67df4
aniledev <elinamcgill@gmail.com>
2021-04-10 00:46:48 -0400

    CHORE: edit create-react-app boilerplate for initial deployment to vercel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import "unfetch/polyfill";
+// import "unfetch/polyfill";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./contexts/UserContext";
-import App from "./components/App/App";
-import "./setup-icons";
+// import { UserProvider } from "./contexts/UserContext";
+import App from "./App";
+// import "./setup-icons";
 import "./index.css";
-import * as serviceWorker from "./serviceWorker";
+// import * as serviceWorker from "./serviceWorker";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
+    {/* <UserProvider> */}
+    <App />
+    {/* </UserProvider> */}
   </BrowserRouter>,
   document.getElementById("root")
 );
