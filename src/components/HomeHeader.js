eafc26870c89c0eb6a9cc8b078000b3cd67925ab
aniledev <elinamcgill@gmail.com>
2021-04-10 14:08:20 -0400

    STYLES: add styling to restaurant home page

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -3,8 +3,8 @@ import React, { Component } from "react";
 export default class HomeHeader extends Component {
   render() {
     return (
-      <div>
-        <h1 className="font-weight-light text-center">Bon Appetit</h1>
+      <div className="HomeHeader">
+        <h1 className="font-weight-light display-1 text-center">Bon Appetit</h1>
       </div>
     );
   }
--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import HomeHeader from "../components/HomeHeader";
 import AddRestaurantForm from "../components/AddRestaurantForm";
 import RestaurantList from "../components/RestaurantList";
+import "../styles/AddRestaurantForm.css";
 
 export default class HomeRoute extends Component {
   render() {
