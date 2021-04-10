24c0902362148b085309cc630b7fb695717b15cd
aniledev <elinamcgill@gmail.com>
2021-04-10 14:40:15 -0400

    CHORE: wrap app in Context.Provider

--- src/App.js
+++ src/App.js
@@ -6,22 +6,30 @@ import LandingRoute from "./routes/LandingRoute";
 import UpdateRoute from "./routes/UpdateRoute";
 import NotFoundRoute from "./routes/NotFoundRoute";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { ContextProvider } from "./context";
 
+// make sure to wrap App in context so that it can consume the context object
 function App() {
   return (
-    <div className="App container">
-      <Switch>
-        <Route exact path={"/"} component={LandingRoute} />
-        <Route exact path={"/home"} component={HomeRoute} />
-        <Route
-          exact
-          path={"/restaurants/:id"}
-          component={RestaurantDetailRoute}
-        />
-        <Route exact path={"/restaurants/:id/update"} component={UpdateRoute} />
-        <Route component={NotFoundRoute} />
-      </Switch>
-    </div>
+    <ContextProvider>
+      <div className="App container">
+        <Switch>
+          <Route exact path={"/"} component={LandingRoute} />
+          <Route exact path={"/home"} component={HomeRoute} />
+          <Route
+            exact
+            path={"/restaurants/:id"}
+            component={RestaurantDetailRoute}
+          />
+          <Route
+            exact
+            path={"/restaurants/:id/update"}
+            component={UpdateRoute}
+          />
+          <Route component={NotFoundRoute} />
+        </Switch>
+      </div>
+    </ContextProvider>
   );
 }
 
