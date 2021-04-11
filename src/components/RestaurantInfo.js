d268220e8cb402eb3b2e2d348b5b0486e4c03ebf
aniledev <elinamcgill@gmail.com>
2021-04-11 03:35:59 -0400

    CHORE: write pseudocode for http request

--- src/components/RestaurantInfo.js
+++ src/components/RestaurantInfo.js
@@ -5,7 +5,10 @@ import { Context } from "../context";
 const RestaurantInfo = () => {
   // destructure the params object
   // destructure the context object
-  // make a call to the server api/restaurant/:id
+  // make a call to the server api/restaurant/:id using useEffect
+  // wrap in a try catch bloc
+  // make a get request to /api/restaurant/:id
+  // save this repsonse to the state
 
   return <div className="Restaurant">Restaurant Info Page</div>;
 };
