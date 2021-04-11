c6c9d068311a29b517a567e2491cf10b6951867e
aniledev <elinamcgill@gmail.com>
2021-04-11 04:40:36 -0400

    CHORE: write pseducode for Rating component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -1,6 +1,11 @@
 import React from "react";
 
 const Rating = () => {
+  // create an array to hold the number of stars
+  // loop through the rating passed in from props
+  // if i <= rating then add a start to the star array
+  // render the star array
+
   return <div className="Rating"></div>;
 };
 
