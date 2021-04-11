46097446abda7e0bb26da48b64b361868e546de3
aniledev <elinamcgill@gmail.com>
2021-04-11 02:09:44 -0400

    FEAT: wire up useParams for use in routing

--- src/components/UpdateForm.js
+++ src/components/UpdateForm.js
@@ -1,6 +1,10 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 const UpdateForm = () => {
+  // use params in order to route the page to the update form for a specific restaurant
+  const test = useParams();
+  console.log(test);
   return <div className="UpdateForm">Update Form</div>;
 };
 
