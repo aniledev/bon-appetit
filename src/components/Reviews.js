aef74c8eb3956283064d67fe748a12a2741d13f6
aniledev <elinamcgill@gmail.com>
2021-04-11 05:47:28 -0400

    FEAT: flesh out JSX for Review box

---
+++ src/components/Reviews.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Rating from "./Rating";
+
+const Reviews = () => {
+  return (
+    <div className="Reviews row row-cols-3 mb-2">
+      <div
+        className="card text-white bg-primary mb-3 mr-4"
+        style={{ maxWidth: "30%" }}
+      >
+        <div className="card-header d-flex justify-content-between">
+          <span>Elina</span>
+          <spain>
+            <Rating rating={3} />
+          </spain>
+        </div>
+        <div className="card-body">
+          <p className="card-text">Some text here</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Reviews;
