7f14a4bdb886991672757e5a22e097cee5b6ace5
aniledev <elinamcgill@gmail.com>
2021-04-11 03:25:24 -0400

    CHORE: create selectedRestaurant property in context object for detail page

--- src/context.js
+++ src/context.js
@@ -4,13 +4,22 @@ export const Context = createContext();
 
 export const ContextProvider = (props) => {
   const [restaurants, setRestaurants] = useState([]);
+  const [selectedRestaurant, setSelectedRestaurant] = useState([]);
   const addRestaurant = (restaurant) => {
     setRestaurants([...restaurants, restaurant]);
   };
   return (
     // the context provider needs to hold the value of restaurants from the api
     // setRestaurants is a function that updates the state with the restaurant response data
-    <Context.Provider value={{ restaurants, setRestaurants, addRestaurant }}>
+    <Context.Provider
+      value={{
+        restaurants,
+        setRestaurants,
+        addRestaurant,
+        selectedRestaurant,
+        setSelectedRestaurant,
+      }}
+    >
       {props.children}
     </Context.Provider>
   );
