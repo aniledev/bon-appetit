877d9baece4c60019a26ebcd9129360a1b04c63c
aniledev <elinamcgill@gmail.com>
2021-04-11 03:33:20 -0400

    FEAT: create child component for Restaurant Detail Route

---
+++ src/components/RestaurantInfo.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { useParams } from "react-router-dom";
+import { Context } from "../context";
+
+const RestaurantInfo = () => {
+  // destructure the params object
+  // destructure the context object
+  // make a call to the server api/restaurant/:id
+
+  return <div className="Restaurant">Restaurant Info Page</div>;
+};
+
+export default RestaurantInfo;
--- src/routes/RestaurantDetailRoute.js
+++ src/routes/RestaurantDetailRoute.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
+import RestaurantInfo from "../components/RestaurantInfo";
 
 export default class RestaurantRoute extends Component {
   render() {
-    return <div className="RestaurantRoute">Restaurant Detail Route</div>;
+    return (
+      <div className="RestaurantRoute">
+        <RestaurantInfo />
+      </div>
+    );
   }
 }
