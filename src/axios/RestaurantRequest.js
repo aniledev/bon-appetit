e169c46c5245b092e485789df2ccb26a46ab11c1
aniledev <elinamcgill@gmail.com>
2021-04-11 09:45:21 -0400

    CHORE: update config vars for deployment

--- src/axios/RestaurantRequest.js
+++ src/axios/RestaurantRequest.js
@@ -1,6 +1,7 @@
 // use this file to set up axios to handle our requests to the server
 import axios from "axios";
+import config from "../config";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: config.API_ENDPOINT,
 });
