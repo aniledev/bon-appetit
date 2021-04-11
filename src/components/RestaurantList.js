797033f556b0939fb58b06d91830fd2121dec5b4
aniledev <elinamcgill@gmail.com>
2021-04-10 23:36:32 -0400

    REFRACTOR: change class component into function component for hooks use

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,85 +1,79 @@
-import React, { Component } from "react";
-// import axios request config module
+import React, { useEffect } from "react";
+import RestaurantRequest from "../axios/RestaurantRequest";
 
-export default class RestaurantList extends Component {
+// useEffeect doesnt wok with class components refractor into function component first
+
+const RestaurantList = () => {
   // use effect runs when the component mounts
   // use async /await actions to make request to restaurant
   // due to async actions wrap your code in a try catch block for error handling
 
-  render() {
-    return (
-      <div className="RestaurantList list-group">
-        <table className="table table-hover table-light">
-          <thead>
-            <tr className="bg-light">
-              <th scope="col">Restaurant</th>
-              <th scope="col">Location</th>
-              <th scope="col">Price Range</th>
-              <th scope="col">Rating</th>
-              <th scope="col">Edit</th>
-              <th scope="col">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Mellow Mushroom</td>
-              <td>Winter Park</td>
-              <td>$$</td>
-              <td>Rating</td>
-              <td>
-                <button className="btn btn-warning update-button">
-                  Update
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-info delete-button">Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td>Mellow Mushroom</td>
-              <td>Winter Park</td>
-              <td>$$</td>
-              <td>Rating</td>
-              <td>
-                <button className="btn btn-warning update-button">
-                  Update
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-info delete-button">Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td>Mellow Mushroom</td>
-              <td>Winter Park</td>
-              <td>$$</td>
-              <td>Rating</td>
-              <td>
-                <button className="btn btn-warning update-button">
-                  Update
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-info delete-button">Delete</button>
-              </td>
-            </tr>
-            <tr>
-              <td>Mellow Mushroom</td>
-              <td>Winter Park</td>
-              <td>$$</td>
-              <td>Rating</td>
-              <td>
-                <button className="btn btn-warning update-button">
-                  Update
-                </button>
-              </td>
-              <td>
-                <button className="btn btn-info delete-button">Delete</button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="RestaurantList list-group">
+      <table className="table table-hover table-light">
+        <thead>
+          <tr className="bg-light">
+            <th scope="col">Restaurant</th>
+            <th scope="col">Location</th>
+            <th scope="col">Price Range</th>
+            <th scope="col">Rating</th>
+            <th scope="col">Edit</th>
+            <th scope="col">Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Mellow Mushroom</td>
+            <td>Winter Park</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning update-button">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-info delete-button">Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Mellow Mushroom</td>
+            <td>Winter Park</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning update-button">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-info delete-button">Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Mellow Mushroom</td>
+            <td>Winter Park</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning update-button">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-info delete-button">Delete</button>
+            </td>
+          </tr>
+          <tr>
+            <td>Mellow Mushroom</td>
+            <td>Winter Park</td>
+            <td>$$</td>
+            <td>Rating</td>
+            <td>
+              <button className="btn btn-warning update-button">Update</button>
+            </td>
+            <td>
+              <button className="btn btn-info delete-button">Delete</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default RestaurantList;
